d76f7649331f8d9603a8c5b8d74c3beb589be99b
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-02-22 00:32:55 +0100

    Voiceover working completely

--- pile/shell.nix
+++ pile/shell.nix
@@ -2,8 +2,17 @@ with import (fetchTarball https://github.com/FaustXVI/nixpkgs/archive/manim-voic
 
 pkgs.mkShell {
   name = "pile-env";
-  packages = [ 
+  packages = [
+    sox
     jetbrains.pycharm-professional
     (python3.withPackages (p: with p;[ manim-voiceover manim]))
+    texlive.combined.scheme-full
+    gst_all_1.gstreamer
+    intltool
+    gobject-introspection
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-ugly
 ];
 }
