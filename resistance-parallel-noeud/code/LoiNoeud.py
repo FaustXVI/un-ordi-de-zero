d6b748007cf2ab514732e7349ef2677d5e69edcc
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2024-01-22 00:13:29 +0100

    Animation ready

--- resistance-parallel-noeud/code/LoiNoeud.py
+++ resistance-parallel-noeud/code/LoiNoeud.py
@@ -1,13 +1,13 @@
-from manim import ORIGIN as origin, LEFT as left, PI as pi, UP as up, DOWN as down, RIGHT as right, DARK_BLUE
+import random
+
 from manim.__main__ import main
 from MyScene import MyScene
 import locale
 from electronics import *
-from electronics import Circuit, Battery, Resistance, Ameter, Branch, Junction, Contact
 
 # locale.setlocale(locale.LC_ALL, 'fr_FR')
 
-section_done = True
+section_done = False
 
 
 class LoiNoeud(MyScene):
@@ -18,16 +18,17 @@ class LoiNoeud(MyScene):
     def construct(self):
         self.next_section(skip_animations=section_done)
         node = Dot()
-        lineI1 = Line(start=LEFT * 3 + UP * 3, end=ORIGIN)
+        lineFactor = 4.5
+        lineI1 = Line(start=LEFT * lineFactor + UP * lineFactor, end=ORIGIN)
         i1 = Arrow(start=LEFT + UP, end=ORIGIN).move_to(LEFT * 2 + UP * 2.5)
         i1Label = MathTex("i_1").move_to(i1.get_center() + (RIGHT + UP) * 0.25)
-        lineI2 = Line(start=LEFT * 3 + DOWN * 3, end=ORIGIN)
+        lineI2 = Line(start=LEFT * lineFactor + DOWN * lineFactor, end=ORIGIN)
         i2 = Arrow(start=LEFT + DOWN, end=ORIGIN).move_to(LEFT * 2 + DOWN * 2.5)
         i2Label = MathTex("i_2").move_to(i2.get_center() + (RIGHT + DOWN) * 0.25)
-        lineI3 = Line(end=RIGHT * 3 + UP * 3, start=ORIGIN)
+        lineI3 = Line(end=RIGHT * lineFactor + UP * lineFactor, start=ORIGIN)
         i3 = Arrow(end=RIGHT + UP, start=ORIGIN).move_to(RIGHT * 2 + UP * 2.5)
         i3Label = MathTex("i_3").move_to(i3.get_center() + (LEFT + UP) * 0.25)
-        lineI4 = Line(end=RIGHT * 3 + DOWN * 3, start=ORIGIN)
+        lineI4 = Line(end=RIGHT * lineFactor + DOWN * lineFactor, start=ORIGIN)
         i4 = Arrow(end=RIGHT + DOWN, start=ORIGIN).move_to(RIGHT * 2 + DOWN * 2.5)
         i4Label = MathTex("i_4").move_to(i4.get_center() + (LEFT + DOWN) * 0.25)
         with self.my_voiceover(
@@ -52,32 +53,42 @@ class LoiNoeud(MyScene):
                 AnimationGroup(Create(i3), Create(i3Label), lag_ratio=0.66),
                 AnimationGroup(Create(i4), Create(i4Label), lag_ratio=0.66), lag_ratio=0.66
             ), duration=timer.duration)
-        self.next_section(skip_animations=False)
+        self.next_section(skip_animations=section_done)
         with self.my_voiceover(
                 """
                 La loi des nœuds dit que tout ce qui entre dans un nœud doit sortir.
  
- C'est ce que l'on appelle la conservation des charges électriques et c'est quelque chose qui était sous entendu et nécessaire dans mon explication de la loi des mailles.
+ C'est ce que l'on appelle la conservation des charges électriques et c'est quelque chose qui était sous entendu 
+ et nécessaire dans mon explication de la loi des mailles.
                 """) as timer:
-            # multiples electrons entering / exiting from différent points
-            self.wait(timer.duration)
-        self.next_section(skip_animations=True)
+            electrons = [Dot(color=YELLOW) for i in range(0, timer.duration * 4)]
+            animations = [Succession(
+                MoveAlongPath(e, random.choice([lineI1, lineI2]), rate_functions=linear),
+                MoveAlongPath(e, random.choice([lineI3, lineI4]), rate_functions=linear)
+                , rate_functions=linear
+            ) for e in electrons]
+            self.play(AnimationGroup(*animations, lag_ratio=0.2, rate_functions=linear), rate_functions=linear,
+                      duration=timer.duration)
+        self.next_section(skip_animations=section_done)
+        simpleSum = MathTex("i_1", "+", "i_2", "=", "i_3", "+", "i_4")
         with self.my_voiceover(
                 """Ça veut dire que la somme des intensités entrantes est égale à l'opposé de la somme des intensités sortantes.""") as timer:
-            # i_1 + i_2 = i_3 + i_4
-            self.wait(timer.duration)
+            self.play(Succession(AnimationGroup(*[FadeOut(o) for o in self.mobjects]), Create(simpleSum)),
+                      duration=timer.duration)
+        self.next_section(skip_animations=section_done)
+        oppositeSum = MathTex("i_1", "+", "i_2", "=", "(-", "i_3", ")", "+ ", "(-", "i_4", ")")
         with self.my_voiceover(
                 """Cependant, une intensité peut être mesurée dans les deux sens, on peut donc tout mesurer comme si on avait que des intensités entrantes. On a juste à inverser i3 et i4""") as timer:
-            # i_1 + i_2 = (-i_3) + (-i_4)
-            self.wait(timer.duration)
+            self.play(TransformMatchingTex(simpleSum, oppositeSum))
+        flippedSum = MathTex("i_1", "+", "i_2", "+ ", "i_3", "+ ", "i_4", "=", "0")
         with self.my_voiceover(
                 """Si on passe i3 et i4 de l'autre côté, on obtient""") as timer:
-            # i_1 + i_2 + i_3 + i_4 = 0
-            self.wait(timer.duration)
+            self.play(TransformMatchingTex(oppositeSum, flippedSum))
+        self.next_section(skip_animations=False)
+        finalSum = MathTex("\sum_{n}{", "i_n", "}", "=", "0")
         with self.my_voiceover(
                 """Si on généralise, on retrouve la vrai formulation de la loi des nœuds : la sommes des intensités en un point, toutes mesurées dans le même sens par rapport au nœud, est égal à zéro""") as timer:
-            # \sum_{n}{i_n} = 0
-            self.wait(timer.duration)
+            self.play(TransformMatchingTex(flippedSum, finalSum, key_map={"i_4": "i_n"}))
         self.next_section(skip_animations=False)
         self.wait(3)
         self.play(AnimationGroup(*[FadeOut(o) for o in self.mobjects]))
