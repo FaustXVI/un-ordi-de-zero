5511dd7fe1b5087ef5a9973db6c9c6ea79e49f99
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2024-07-28 18:01:10 +0200

    elec moving

--- condensator/code/TruthTable.py
+++ condensator/code/TruthTable.py
@@ -1,3 +1,4 @@
+import functools
 import random
 
 from manim.__main__ import main
@@ -8,7 +9,88 @@ from electronics import *
 # locale.setlocale(locale.LC_ALL, 'fr_FR')
 
 section_done = False
-recording = True
+recording = False
+
+atomsPerUnit = 3
+atomSize = 1.5
+
+
+class AtomState(Enum):
+    NEUTRAL = 1
+    POSITIVE = 2
+    NEGATIVE = 3
+
+
+def colorByState(state):
+    if state == AtomState.POSITIVE:
+        return RED_D
+    if state == AtomState.NEGATIVE:
+        return BLUE_D
+    return YELLOW_D
+
+
+def drawAtoms(atoms):
+    return VGroup(*[
+        Dot(color=colorByState(atom.state)).scale(atomSize).move_to(ORIGIN + atom.position) for atom in atoms
+    ],
+                  *[
+                      Text("-").scale(atomSize * 0.33).move_to(ORIGIN + atom.position) for atom in atoms if
+                      atom.state == AtomState.NEGATIVE
+                  ],
+                  *[
+                      Text("+").scale(atomSize * 0.33).move_to(ORIGIN + atom.position) for atom in atoms if
+                      atom.state == AtomState.POSITIVE
+                  ], )
+
+
+def createAnimationForNextStep(previousAtoms, newAtoms):
+    return AnimationGroup(FadeOut(previousAtoms), FadeIn(newAtoms))
+
+
+def getX(position):
+    return position[0]
+
+
+def getY(position):
+    return position[1]
+
+
+class Atom:
+    def __init__(self, position, state):
+        self.position = position
+        self.state = state
+
+    def closeTo(self, atom):
+        return ((abs(getX(self.position) - getX(atom.position)) <= 1 and getY(self.position) == getY(
+            atom.position)) or
+                (abs(getY(self.position) - getY(atom.position)) <= 1 and getX(self.position) == getX(
+                    atom.position)))
+
+
+def createRectangle(start, stop):
+    (startx, starty) = start
+    (stopx, stopy) = stop
+    return [Atom((x / atomsPerUnit, y / atomsPerUnit, 0), AtomState.NEUTRAL) for x in
+            range(startx * atomsPerUnit, (stopx + 1) * atomsPerUnit) for y in
+            range(starty * atomsPerUnit, (stopy + 1) * atomsPerUnit)]
+
+
+def computeNextAtoms(atoms):
+    newAtoms = []
+    for atom in atoms:
+        if atom.position not in [a.position for a in newAtoms]:
+            currentState = atom.state
+            if currentState == AtomState.NEGATIVE:
+                neighbours = [n for n in atoms if
+                              n.closeTo(atom) and (n.position not in [a.position for a in newAtoms])]
+                newStatePosition = random.choice(neighbours)
+                print(newStatePosition.position)
+                if newStatePosition.position == atom.position:
+                    newAtoms.append(Atom(atom.position, atom.state))
+                else:
+                    newAtoms.append(Atom(atom.position, newStatePosition.state))
+                    newAtoms.append(Atom(newStatePosition.position, atom.state))
+    return [*newAtoms,*[n for n in atoms if n.position not in [a.position for a in newAtoms]]]
 
 
 class TruthTable(MyScene):
@@ -18,149 +100,22 @@ class TruthTable(MyScene):
 
     def construct(self):
         self.next_section(skip_animations=section_done)
-        affirm = Text("""Du courant passe.""").shift(UP * 2)
+        r1 = createRectangle((-1, -1), (1, 1))
+        r1[0].state = AtomState.NEGATIVE
+        atoms = drawAtoms(r1)
         with self.my_voiceover(
-                r"""Tout ce qui va suivre tourne autour de l'affirmation suivante : `Du courant passe.`""") as timer:
-            self.play(Write(affirm), run_time=timer.duration)
-        self.next_section(skip_animations=section_done)
-        bat = Battery().shift(UP).rotate(PI)
-        switch = Switch().shift(DOWN + LEFT)
-        r = Resistance().shift(DOWN + RIGHT)
-        circuit = Circuit(
-            bat,
-            bat.connect(switch),
-            switch,
-            switch.connect(r),
-            r,
-            r.connect(bat)
-        )
+                r"""TODO""") as timer:
+            self.play(FadeIn(atoms), run_time=timer.duration)
+
+        steps = functools.reduce(lambda acc, _: [*acc, computeNextAtoms(acc[-1])], range(1, 50), [r1])
+        drawings = [drawAtoms(atoms) for atoms in steps]
+        animations = [createAnimationForNextStep(p, n) for (p, n) in zip(drawings, drawings[1:])]
         with self.my_voiceover(
-                r"""Ainsi, si on prends un circuit simple contenant juste une pile, un interrupteur et une résistance. """) as timer:
-            self.play(Create(circuit), run_time=timer.duration)
-        faux = Text("Faux")
-        vrai = Text("Vrai")
-        with self.my_voiceover(
-                r"""Quand l'interrupteur est ouvert, l'affirmation est `Fausse`, car aucun courant ne passe.""") as timer:
-            self.play(Write(faux), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""Quand l'interrupteur est fermé,""") as timer:
-            self.play(AnimationGroup(switch.animate.close(), FadeOut(faux)), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""l'affirmation est Vrai, car du courant passe.""") as timer:
-            self.play(AnimationGroup(circuit.run_electron(), Write(vrai)), run_time=timer.duration)
-        self.next_section(skip_animations=section_done)
-        with self.my_voiceover(
-                r"""Tout se joue donc sur l'état de l'interrupteur.""") as timer:
-            self.play(AnimationGroup(*[FadeOut(o) for o in self.mobjects if o != switch],
-                                     switch.animate.move_to(ORIGIN + (LEFT * 4) + UP), run_time=timer.duration))
-        faux.next_to(switch, RIGHT)
-        with self.my_voiceover(
-                r"""Quand il est ouvert, on est dans l'état `Faux` : le courant ne passe pas""") as timer:
-            self.play(AnimationGroup(switch.animate.open(), Write(faux), run_time=timer.duration))
-        switch2 = switch.copy()
-        self.play(switch2.animate.move_to(ORIGIN + (LEFT * 4) + DOWN))
-        vrai.next_to(switch2, RIGHT)
-        with self.my_voiceover(
-                r"""Quand il est fermé, on est dans l'état `Vrai` : le courant passe""") as timer:
-            self.play(AnimationGroup(switch2.animate.close(), Write(vrai), run_time=timer.duration))
-        self.next_section(skip_animations=section_done)
-        switch3 = switch.copy()
-        switch4 = switch2.copy()
-        faux2 = faux.copy()
-        vrai2 = vrai.copy()
-        c1 = VGroup(switch3, faux2)
-        c2 = VGroup(switch4, vrai2)
-        with self.my_voiceover(
-                r"""On peut maintenant regarder ce qu'il se passe quand on met deux interrupteurs dans le circuit en faisant un tableau avec les états du premier interrupteur en lignes et les états du second en colonnes.""") as timer:
-            self.play(
-                AnimationGroup(c1.animate.move_to(ORIGIN + UP * 2), c2.animate.move_to(ORIGIN + UP * 2 + (RIGHT * 4)),
-                               run_time=timer.duration))
-        with self.my_voiceover(
-                r"""En logique, on appelle ces tableaux des tables de vérités et seul les états `Vrai` et `Faux` nous interessent.""") as timer:
-            self.play(AnimationGroup(FadeOut(switch), FadeOut(switch2), FadeOut(switch3), FadeOut(switch4)),
-                      run_time=timer.duration)
-        self.next_section(skip_animations=section_done)
-        l1 = Line(UP * 2, DOWN * 2).shift(LEFT * 1.5)
-        l2 = Line(UP * 2, DOWN * 2).shift(RIGHT * 1.5)
-        l3 = Line(LEFT * 5, RIGHT * 5).shift(UP * 0.75)
-        l4 = Line(LEFT * 5, RIGHT * 5).shift(DOWN * 0.75)
-        lines = VGroup(l1, l2, l3, l4)
-        headers = VGroup(faux, faux2, vrai, vrai2)
-        table = VGroup(lines, headers)
-        tableLine1 = ORIGIN
-        tableCol1 = ORIGIN
-        tableLine2 = DOWN * 1.5
-        tableCol2 = RIGHT * 3
-        lineHeaderShift = LEFT * 3
-        colHeaderShift = UP * 1.5
-        with self.my_voiceover(
-                r"""Et chaque opération logique, comme le `ET` ou le `OU`, on leur propre table de vérité.""") as timer:
-            self.play(
-                Succession(AnimationGroup(
-                    faux.animate.move_to(lineHeaderShift + tableLine1),
-                    vrai.animate.move_to(lineHeaderShift + tableLine2),
-                    faux2.animate.move_to(colHeaderShift + tableCol1),
-                    vrai2.animate.move_to(colHeaderShift + tableCol2)
-                ),
-                    Create(lines)), run_time=timer.duration)
-        self.next_section(skip_animations=section_done)
-        et = Text("ET").move_to(lineHeaderShift + colHeaderShift + tableCol1 + tableLine1)
-        ou = Text("OU").move_to(lineHeaderShift + colHeaderShift + tableCol1 + tableLine1)
-        f1 = Text("Faux").move_to(UP*10)
-        f2 = Text("Faux").move_to(UP*10)
-        f3 = Text("Faux").move_to(UP*10)
-        v1 = Text("Vrai").move_to(UP*10)
-        v2 = Text("Vrai").move_to(UP*10)
-        v3 = Text("Vrai").move_to(UP*10)
-        values = VGroup(f1, f2, f3, v1, v2, v3)
-        with self.my_voiceover(
-                r"""Par exemple, pour l'opération `ET` on obtient""") as timer:
-            self.play(Write(et), run_time=timer.duration)
-        f1.move_to(tableCol1 + tableLine1)
-        f2.move_to(tableCol2 + tableLine1)
-        f3.move_to(tableCol1 + tableLine2)
-        v1.move_to(tableCol2 + tableLine2)
-        with self.my_voiceover(
-                r"""`FAUX` et `FAUX` donne `FAUX`""") as timer:
-            self.play(Write(f1), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`FAUX` et `VRAI` donne `FAUX`""") as timer:
-            self.play(Write(f2), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`VRAI` et `FAUX` donne `FAUX`""") as timer:
-            self.play(Write(f3), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`VRAI` et `VRAI` donne `VRAI`""") as timer:
-            self.play(Write(v1), run_time=timer.duration)
-        self.next_section(skip_animations=False)
-        with self.my_voiceover(
-                r"""Ce qui correspond bien à ce qu'on avait observé lors de la création de notre porte `ET`.""") as timer:
-            self.play(Wait(), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""Si maintenant on regarde l'opération `OU` on obtient""") as timer:
-            self.play(AnimationGroup(FadeOut(values), Transform(et, ou)), run_time=timer.duration)
-        f1.move_to(tableCol1 + tableLine1)
-        v1.move_to(tableCol2 + tableLine1)
-        v2.move_to(tableCol1 + tableLine2)
-        v3.move_to(tableCol2 + tableLine2)
-        with self.my_voiceover(
-                r"""`FAUX` ou `FAUX` donne `FAUX`""") as timer:
-            self.play(Write(f1), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`FAUX` ou `VRAI` donne `VRAI`""") as timer:
-            self.play(Write(v1), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`VRAI` ou `FAUX` donne `VRAI`""") as timer:
-            self.play(Write(v2), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""`VRAI` ou `VRAI` donne `VRAI`""") as timer:
-            self.play(Write(v3), run_time=timer.duration)
-        with self.my_voiceover(
-                r"""Ce qui correspond bien à notre circuit actuel.""") as timer:
-            self.play(Wait(), run_time=timer.duration)
-        self.next_section(skip_animations=False)
-        self.wait(3)
-        self.play(AnimationGroup(*[FadeOut(o) for o in self.mobjects]))
+                r"""TODO""") as timer:
+            for drawing in drawings:
+                self.clear()
+                self.add(drawing)
+                self.wait(0.2)
 
 
 if __name__ == "__main__":
