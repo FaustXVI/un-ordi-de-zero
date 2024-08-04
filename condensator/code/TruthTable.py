2755bb3082d52b5b1e6d9c1076d8e64dc3ae143c
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2024-08-04 16:25:08 +0200

    vault backup: 2024-08-04 16:25:08

--- condensator/code/TruthTable.py
+++ condensator/code/TruthTable.py
@@ -9,7 +9,7 @@ from electronics import *
 
 # locale.setlocale(locale.LC_ALL, 'fr_FR')
 
-section_done = False
+section_done = True
 recording = False
 
 frame_factor = 3
@@ -71,12 +71,19 @@ class Atom:
         return self.distance(atom) <= 1
 
 
-def createRectangle(start, stop):
+def createRectangle(start, stop, state=AtomState.NEUTRAL):
     (startx, starty) = start
     (stopx, stopy) = stop
-    return [Atom((x, y, 0), AtomState.NEUTRAL) for x in
+    return [Atom((x, y, 0), state) for x in
             range(startx, (stopx + 1)) for y in
             range(starty, (stopy + 1))]
+
+
+def countStateInRectangle(start, stop, atoms, state):
+    (startx, starty) = start
+    (stopx, stopy) = stop
+    positions = [(x, y, 0) for x in range(startx, (stopx + 1)) for y in range(starty, (stopy + 1))]
+    return sum([1 for a in atoms if a.state == state and (a.position in positions)])
 
 
 def computeNextAtoms(atoms):
@@ -143,7 +150,7 @@ def computeNextAtoms(atoms):
 
 def simulate(circuit, nbFrames):
     steps = functools.reduce(lambda acc, _: [*acc, computeNextAtoms(acc[-1])], range(1, nbFrames), [circuit])
-    return [drawAtoms(atoms) for atoms in steps]
+    return ([drawAtoms(atoms) for atoms in steps], steps[-1])
 
 
 def constructLeftSide(distance=0):
@@ -154,15 +161,20 @@ def createLeftPlate(distance):
     return createRectangle((-3 - distance, -5), (-1 - distance, 5))
 
 
+def countNegativesInLeftPlate(distance, atoms):
+    return countStateInRectangle((-3 - distance, -5), (-1 - distance, 5), atoms, AtomState.NEGATIVE)
+
+
 def createLeftCable(distance):
     return createRectangle((-10 - distance, -1), (-4 - distance, 1))
 
 
+def createDisconnectedLeftCable(distance):
+    return createRectangle((-9 - distance, -1), (-3 - distance, 1))
+
+
 def createLeftBattery(distance):
-    leftBattery = createRectangle((-15 - distance, -3), (-11 - distance, 3))
-    for a in leftBattery:
-        a.state = AtomState.NEGATIVE
-    return leftBattery
+    return createRectangle((-15 - distance, -3), (-11 - distance, 3), AtomState.NEGATIVE)
 
 
 def constructRightSide(distance=0):
@@ -178,10 +190,7 @@ def createRightCable(distance):
 
 
 def createRightBattery(distance):
-    rightBattery = createRectangle((11 + distance, -3), (15 + distance, 3))
-    for a in rightBattery:
-        a.state = AtomState.POSITIVE
-    return rightBattery
+    return createRectangle((11 + distance, -3), (15 + distance, 3), AtomState.POSITIVE)
 
 
 def constructCircuit(distance=0):
@@ -195,23 +204,32 @@ class TruthTable(MyScene):
 
     def playSimulation(self, circuit, duration):
         nbFrames = math.ceil(duration / timeBetweenFrames)
-        drawings = simulate(circuit, nbFrames)
+        (drawings, lastState) = simulate(circuit, nbFrames)
         for (i, drawing) in enumerate(drawings):
             if (i % SPEED_UP == 0):
                 self.clear()
                 self.add(drawing)
                 self.wait(timeBetweenFrames)
+        return lastState
 
     def construct(self):
+        self.next_section(skip_animations=section_done)
+        leftBattery = createLeftBattery(3)
+        leftBatteryDrawing = drawAtoms(leftBattery)
+        with self.my_voiceover(r"""TODO""") as timer:
+            self.play(FadeIn(leftBatteryDrawing), run_time=timer.duration)
         self.next_section(skip_animations=section_done)
         circuit = constructCircuit(MAX_DISTANCE_EFFECT)
         with self.my_voiceover(
                 r"""TODO""", duration=10) as timer:
-            self.playSimulation(circuit, timer.duration)
-        circuit = constructCircuit()
-        with self.my_voiceover(
-                r"""TODO""", duration=10) as timer:
-            self.playSimulation(circuit, timer.duration)
+            finalState = self.playSimulation(circuit, timer.duration)
+        negativeInLeftPlate = countNegativesInLeftPlate(MAX_DISTANCE_EFFECT, finalState)
+        with self.my_voiceover(f"""On a {negativeInLeftPlate}""") as timer:
+            self.wait(timer.duration)
+        # circuit = constructCircuit()
+        # with self.my_voiceover(
+        #         r"""TODO""", duration=10) as timer:
+        #     self.playSimulation(circuit, timer.duration)
 
 
 if __name__ == "__main__":
